61bd970c791a8056942aa30448cf5c9a0a0d8ef6
Rahul Mahajan <aerorahul@users.noreply.github.com>
2025-08-26 11:14:15 -0400

    update gitignore based on reviewer comments

---
+++ src/mlglobal/__init__.py
@@ -0,0 +1,16 @@
+# This file marks the directory as a Python package.
+"""MLGlobal: Machine Learning Global Applications
+
+This package implements Google DeepMind GraphCast for the Global Applications
+such as the Global Forecast System (GFS) and Global Ensemble Forecast System (GEFS)
+as part of the Experimental AI Global and Limited-area Ensemble (EAGLE) project.
+
+For detailed usage instructions, see README.md
+"""
+
+__version__ = "0.1.0"
+__author__ = "Linlin Cui, Jun Wang, Sadegh Tabas"
+__email__ = "linlin.cui@noaa.gov, jun.wang@noaa.gov"
+__license__ = "CC-0"
+
+__all__ = ["__version__", "__author__", "__email__", "__license__"]
